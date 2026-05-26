5e02b675cb43e2fb73a8d820330310f197d79ca1
warrens-alt <warrens@bastionflowe.com>
2026-05-26 05:38:41 +0200

    Use predictive analytics workbench

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
-import AdvancedAnalyticsApp from './AdvancedAnalyticsApp';
+import AdvancedAnalyticsAppV2 from './AdvancedAnalyticsAppV2';
 import './styles.css';
 import './unified.css';
 import './visual-analytics.css';
 import './analytics-experience.css';
 
-createRoot(document.getElementById('root')!).render(<AdvancedAnalyticsApp />);
+createRoot(document.getElementById('root')!).render(<AdvancedAnalyticsAppV2 />);
