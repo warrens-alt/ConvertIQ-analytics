30a2a215616b61e530c4d6e1531fcacddb634431
warrens-alt <warrens@bastionflowe.com>
2026-05-26 06:04:45 +0200

    Use single integrated analytics app

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
-import AdvancedAnalyticsAppV5 from './AdvancedAnalyticsAppV5';
+import AdvancedAnalyticsApp from './AdvancedAnalyticsApp';
 import './styles.css';
 import './unified.css';
 import './visual-analytics.css';
 import './analytics-experience.css';
 
-createRoot(document.getElementById('root')!).render(<AdvancedAnalyticsAppV5 />);
+createRoot(document.getElementById('root')!).render(<AdvancedAnalyticsApp />);
