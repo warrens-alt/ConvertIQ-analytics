5549452a13c44960f3f631cbbd337683b14f3756
warrens-alt <warrens@bastionflowe.com>
2026-05-26 05:12:41 +0200

    Import visual analytics styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client';
 import AdvancedAnalyticsApp from './AdvancedAnalyticsApp';
 import './styles.css';
 import './unified.css';
+import './visual-analytics.css';
 
 createRoot(document.getElementById('root')!).render(<AdvancedAnalyticsApp />);
