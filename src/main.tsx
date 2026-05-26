4a7e419ed7e5b231cebb40f4ef27af0868822381
warrens-alt <warrens@bastionflowe.com>
2026-05-26 05:19:37 +0200

    Import analytics experience styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import AdvancedAnalyticsApp from './AdvancedAnalyticsApp';
 import './styles.css';
 import './unified.css';
 import './visual-analytics.css';
+import './analytics-experience.css';
 
 createRoot(document.getElementById('root')!).render(<AdvancedAnalyticsApp />);
