9b1fbf70389f673899f4e61fdfcf000d1f3e6b19
warrens-alt <warrens@bastionflowe.com>
2026-05-26 05:54:24 +0200

    Use advanced analytics V4 wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
-import AdvancedAnalyticsAppV3 from './AdvancedAnalyticsAppV3';
+import AdvancedAnalyticsAppV4 from './AdvancedAnalyticsAppV4';
 import './styles.css';
 import './unified.css';
 import './visual-analytics.css';
 import './analytics-experience.css';
 
-createRoot(document.getElementById('root')!).render(<AdvancedAnalyticsAppV3 />);
+createRoot(document.getElementById('root')!).render(<AdvancedAnalyticsAppV4 />);
